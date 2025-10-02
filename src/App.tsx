659084d28690fa4aa675d720aad70dcb9132136e
sumanthd032 <sumanthd032@gmail.com>
2025-10-02 18:30:01 +0530

    Initial project commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import KolamGallery from './components/KolamGallery';
+import Home from './components/home';
+
+import './App.css';
+import Database from './components/database';
+import ClassifyKolam from './components/ClassifyKolam';
+import RecreateKolam from './components/RecreateKolam';
+import PulliKolam from './components/PulliKolam';
+import OneFiveOne from './components/OneFiveOne';
+import AboutUs from './components/About';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/gallery" element={<KolamGallery />} />
+          <Route path="/database" element={<Database />} />
+          <Route path="/classify" element={<ClassifyKolam />} />
+          <Route path="/recreate" element={<RecreateKolam />} />
+          <Route path="/gallery/pulli" element={<PulliKolam />} />
+          <Route path="/database/1-5-1" element={<OneFiveOne />} />
+          <Route path="/aboutus" element={<AboutUs />} /> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
